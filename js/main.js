8b0c60791e2b33fca240e66519371d2c4b6acb8c
phirotor <52757538+phirotor@users.noreply.github.com>
2026-04-16 00:39:24 -0500

    suiGeneris

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 document.querySelectorAll('.has-children').forEach(item => {
   item.addEventListener('click', function(e){
 
+    // Evita que el link interno interfiera
+    if(e.target.tagName === 'A') return;
+
     if(window.innerWidth < 768){
       const children = this.nextElementSibling;
 
-      children.classList.toggle('open');
+      if (!children) return;
+
       children.classList.toggle('collapsed');
     }
 
