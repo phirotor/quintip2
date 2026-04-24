50bbd1d31cb26d1a8272b22f0918449cf6553168
phirotor <52757538+phirotor@users.noreply.github.com>
2026-04-23 22:53:26 -0500

    heather

--- js/main.js
+++ js/main.js
@@ -14,3 +14,9 @@ document.querySelectorAll('.has-children').forEach(item => {
 
   });
 });
+
+fetch("../components/header.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("header").innerHTML = data;
+  });
